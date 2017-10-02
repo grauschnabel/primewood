c3eaa2c58f634401e049d5e43284b9bf833d808b
Martin Kaffanke <m.kaffanke@gmail.com>
2017-10-02 10:54:17 +0200

    first theme version

---
+++ theme_primewood/__manifest__.py
@@ -0,0 +1,15 @@
+{
+  'name':'Primewood Website Theme',
+    'description': 'The first theme for the primewood website, using hartlpeter.at as inspiration',
+  'version':'0.1',
+  'author':'Martin Kaffanke <m.kaffanke@primewood.at>',
+
+  'data': [
+      'data/theme_data.xml',
+      'views/assets.xml',
+      'views/layout.xml',
+      'views/snippets.xml'
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
