c19a2ebc903d020714676d5917d54362eef6a828
Martin Kaffanke <m.kaffanke@gmail.com>
2017-10-11 14:16:53 +0200

    better description

--- theme_primewood/__manifest__.py
+++ theme_primewood/__manifest__.py
@@ -1,15 +1,24 @@
+# -*- coding: utf-8 -*-
 {
-  'name':'Primewood Website Theme',
-    'description': 'The first theme for the primewood website, using hartlpeter.at as inspiration',
-  'version':'0.1',
-  'author':'Martin Kaffanke <m.kaffanke@primewood.at>',
+    'name':'Primewood Website Theme',
 
-  'data': [
-      'data/theme_data.xml',
-      'views/assets.xml',
-      'views/layout.xml',
-      'views/snippets.xml'
-  ],
-  'category': 'Theme/Creative',
-  'depends': ['website'],
+    'summary':"""The layout for the primewood website.""",
+    
+    'description': """
+    The first theme for the primewood website,
+    using hartlpeter.at as inspiration""",
+    
+    'author':'Martin Kaffanke <m.kaffanke@primewood.at>',
+    'website': 'https://www.primewood.at/',
+    'category': 'Theme/Creative',
+    'version':'0.1',
+
+    'depends': ['website'],
+
+    'data': [
+        'data/theme_data.xml',
+        'views/assets.xml',
+        'views/layout.xml',
+        'views/snippets.xml'
+    ],
 }
